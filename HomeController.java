c7a5592ce97dd13fbc6e799e1f5de439064654b4
Jerry-YYB <156628463+Jerry-YYB@users.noreply.github.com>
2025-04-23 01:15:37 +0800

    Add files via upload

---
+++ HomeController.java
@@ -0,0 +1,54 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.GridPane;
+import javafx.stage.FileChooser;
+import utils.StageContainer;
+
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+
+public class HomeController {
+
+    @FXML
+    public StackPane calendarPane;
+
+    @FXML
+    public void initialize() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/calendar.fxml"));
+            GridPane calendarRoot = loader.load();
+            calendarPane.getChildren().add(calendarRoot);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void handleCsvImport(MouseEvent mouseEvent) {
+        javafx.stage.Stage stage = (javafx.stage.Stage) ((javafx.scene.Node) mouseEvent.getSource()).getScene().getWindow();
+
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open Csv File");
+        File file = fileChooser.showOpenDialog(stage);
+        if (file != null) {
+            openFile(file);
+        }
+    }
+
+    private void openFile(File file) {
+        EventQueue.invokeLater(() -> {
+
+        });
+    }
+
+    public void handleImageClick(MouseEvent mouseEvent) {
+    }
+
+    public void openWritingWindow(MouseEvent mouseEvent) {
+        StageContainer.switchStage("writing");
+    }
+}
